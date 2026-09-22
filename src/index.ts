3db59d9eddf22700982910cd264756135af06ddb
Yusuke Kamiyamane <y@y14e.com>
2026-09-22 10:57:30 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -319,6 +319,7 @@ export function forEachOwnKey(
 }
 
 export function isObject(value: unknown): value is object {
+  // 'typeof null' is 'object', but 'object' type excludes 'null'.
   return typeof value === 'object' && value !== null;
 }
 
